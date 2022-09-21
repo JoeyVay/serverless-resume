5732ad1d8de832bf7cef712e6461b99352505c4a
JoeyVay <joeyvay@gmail.com>
2022-09-21 04:03:18 -0400

    updated PutSiteViewCountFunction to use atomic methods rather then manully incromenting the vlaue returned by python. Also updated SiteCount.html to call the PutSiteViewCountFunction

--- Makefile
+++ Makefile
@@ -8,3 +8,9 @@ deploy-infra:
 
 deploy-site:
 	aws s3 sync ./resume-site s3://serverless-resume-site-bucket-joevay
+
+invoke-get-view-count:
+	sam build && sam local invoke GetSiteViewCountFunction
+
+invoke-put-view-count:
+	sam build && sam local invoke PutSiteViewCountFunction
--- put_site_view_count/put_site_view_count.py
+++ put_site_view_count/put_site_view_count.py
@@ -1,37 +1,17 @@
-import json
 import boto3
-
-#set table name
-table_name = 'joevaycom-site-count'
-
-#make DynamoDB client
-visitor_count_dynamodb_client = boto3.client('dynamodb')
-
-#set key to retrive visitor count
-current_visitor_count_get = {'ID':{'S':'visitors'}}
+dynamodb = boto3.resource('dynamodb', region_name= 'us-east-1')
+table = dynamodb.Table('joevaycom-site-count')
 
 def lambda_handler(event,context):
-    
-    #request current count of viewers from db
-    response = visitor_count_dynamodb_client.get_item(TableName = table_name, Key = current_visitor_count_get)
+    response = table.update_item(TableName='joevaycom-site-count', Key={'ID': 'visitors'},
+        ExpressionAttributeNames = {'#count': 'count'},
+        ExpressionAttributeValues = {':increase': 1,},
+        #UpdateExpression = "SET #count = #count + :increase",
+        UpdateExpression = 'ADD #count :increase',
+        ReturnValues = 'UPDATED_NEW'
+    )
 
-    #parse out and store value as a string
-    current_visitor_count = response['Item']['count']['N']
-
-
-    #current_visitor_count = int(current_visitor_count)
-    #current_visitor_count = current_visitor_count + 1
-    #current_visitor_count = str(current_visitor_count)
-    current_visitor_count = str(int(current_visitor_count) + 1)
-
-    #prepare new visitor count to be stored
-    new_visitor_count = {
-        'ID':{'S':'visitors'}
-        ,'count':{'N':(current_visitor_count)}
-    }
-
-    #store updated item in db
-    visitor_count_dynamodb_client.put_item(TableName = table_name, Item = (new_visitor_count))
+    current_visitor_count = (response['Attributes']['count'])
 
     return {
         "statusCode" : 200,
@@ -41,5 +21,5 @@ def lambda_handler(event,context):
             "Access-Control-Allow-Credentials": "*",    #<<<tighten all of these CORS policies
             "Content-Type": "application/json"
         },
-        "body": "stored value: \"{ \"count\": \"" + (current_visitor_count) + "\" }\""
+        "body": "stored value: " + str(current_visitor_count)
     }
